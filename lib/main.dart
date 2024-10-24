879f4db29509ad015a622513188f7c860f3f59bd
Muhammad Ali <softeng.aliijaz@gmail.com>
2024-10-25 02:20:00 +0500

    folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:synque_work/screens/view_all_products_screen.dart';
+import 'package:synque_work/screens/crud/view_all_products_screen.dart';
 
 void main() {
   runApp(const MyApp());
